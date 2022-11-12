478f7a277a7c9479e6285a2b21048cc67364682a
“Yan-MX <jiangyan0302@gmail.com>
2022-11-12 14:43:42 +0100

    feat: call api every  5 seconds

--- src/view/App.js
+++ src/view/App.js
@@ -1,5 +1,5 @@
 import { query } from "../utility/query";
-import getDepartureData  from "../api/getDepartureData";
+import getDepartureData  from "../api&service/getDepartureData";
 import React, { useState, useEffect } from "react";
 
 function App() {
@@ -8,25 +8,26 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
-    //  console.log("initial data call");
-      let responsedata = await getDepartureData(query);
-     // console.log(typeof(responsedata));
-    //  console.log(responsedata);
-      setData(responsedata);
-    }
-    fetchData();
+      console.log("fetch data called");
+       let responsedata = await getDepartureData(query);
+       setData(responsedata);
+     }
+    const interval = setInterval(() =>fetchData(),
+    5000);
+ 
+    return () => clearInterval(interval);
 
   }, []);
 
   return (
-    data && 
+    data && data.stopPlace &&
     <div>
     <div>
       Total number returned: {data.stopPlace.estimatedCalls.length}
     </div>
     {data.stopPlace.estimatedCalls.map((item, index) => (
       <div key={index}>
-        <li>Bus No. {item.serviceJourney.journeyPattern.line.publicCode}</li>
+        <li>Buss No. {item.serviceJourney.journeyPattern.line.publicCode}</li>
         <li>Destination: {item.destinationDisplay.frontText}</li>
         <li>Aimed: {item.aimedArrivalTime}</li>
         <li>Expected: {item.expectedArrivalTime}</li>
