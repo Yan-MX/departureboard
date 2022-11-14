3fcc7e4dd63e026b33f2d038a4a01b73476317fb
“Yan-MX <jiangyan0302@gmail.com>
2022-11-14 21:22:24 +0100

    feat: UI implementation

--- src/utilities/utility.js
+++ src/utilities/utility.js
@@ -1,4 +1,9 @@
 
  const delay = ms => new Promise(res => setTimeout(res, ms));
+ export function getDelayedMins(time1, time2) {
+    return Math.round(
+      (((Date.parse(time1) - Date.parse(time2)) % 86400000) % 3600000) / 60000
+    );
+  }
 
  export default delay
